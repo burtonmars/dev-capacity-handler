03ea6ea1096baf2f84e519640cdd9d78a8f007d4
UBC Student <burtonm1@students.cs.ubc.ca>
2023-05-23 19:17:42 -0700

    basic FE layout for home page set up before trying to request from DB

---
+++ src/app/components/navbar/navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import style from '../../styles/navbar.module.scss';
+
+function Navbar() {
+  return (
+    <div className={style.navbar__container}>
+        <h1>Devacity</h1>
+    </div>
+  )
+};
+
+export default Navbar;
